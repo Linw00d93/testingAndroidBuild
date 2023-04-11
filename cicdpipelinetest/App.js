da68139abf91e6b31aff46863ba3c4144ce4514c
Linwood <Linwood93@gmail.com>
2023-04-11 11:34:02 -0400

    login screen

--- cicdpipelinetest/App.js
+++ cicdpipelinetest/App.js
@@ -6,34 +6,75 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
-import Splash from './Splash';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from './LoginPage';
+import React, { Component,useState } from 'react';
+//import Splash from './Splash';
+//import LoginPage from './LoginPage';
 
-const Stack = createStackNavigator();
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log(`Email: ${email}, Password: ${password}`);
+    // Here you can implement your login logic
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <Splash/>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginPage} />
-            {/* Add other screens here */}
-        </Stack.Navigator>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        autoCapitalize="none"
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000000',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  image: {
-    width: '20%',
-    height: '20%',
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
